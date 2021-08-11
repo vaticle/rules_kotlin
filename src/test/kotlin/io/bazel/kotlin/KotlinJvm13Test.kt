fb4e68ae56f559eb255f20dfaa103a6333759c6b
Alex Walker <alex@grakn.ai>
2021-08-11 10:58:25 +0100

    Fix warnings about deprecated toLowerCase

--- src/test/kotlin/io/bazel/kotlin/KotlinJvm13Test.kt
+++ src/test/kotlin/io/bazel/kotlin/KotlinJvm13Test.kt
@@ -4,7 +4,7 @@ import org.junit.Test
 
 class KotlinJvm13Test {
     @Test fun testFoo() {
-        when (val foo = "TryIng Mixed Case".toLowerCase()) {
+        when (val foo = "TryIng Mixed Case".lowercase()) {
             "trying mixed case" -> { println(foo) }
             else -> {}
         }
